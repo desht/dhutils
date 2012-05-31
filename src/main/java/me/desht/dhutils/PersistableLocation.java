68009a5b30b7b56bbd9b12c1990a3e551544391e
Des Herriott <des.herriott@gmail.com>
2012-05-31 09:54:07 +0100

    Lazier PersistableLocation implementation. Don't store a Location anymore, just store world name and x,y,z,yaw,pitch value. getLocation() will throw an IllegalStateException if the world name isn't a loaded World.

--- src/main/java/me/desht/dhutils/PersistableLocation.java
+++ src/main/java/me/desht/dhutils/PersistableLocation.java
@@ -9,34 +9,85 @@ import org.bukkit.World;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 
 public class PersistableLocation implements ConfigurationSerializable {
-	private Location location;
-	
-	public PersistableLocation(Location l) {
-		location = l;
+	private final String worldName;
+	private final double x, y, z;
+	private final float pitch, yaw;
+	private boolean savePitchAndYaw;
+
+	public PersistableLocation(Location loc) {
+		worldName = loc.getWorld().getName();
+		x = loc.getX();
+		y = loc.getY();
+		z = loc.getZ();
+		pitch = loc.getPitch();
+		yaw = loc.getYaw();
+		savePitchAndYaw = true;
 	}
-	
+
 	public PersistableLocation(Map<String,Object> map) {
-		World w = Bukkit.getWorld((String) map.get("world"));
-		if (w == null) {
-			throw new IllegalArgumentException("No such world " + map.get("world"));
-		}
-		location = new Location(w, (Double)map.get("x"), (Double)map.get("y"), (Double)map.get("z"));
-		location.setPitch((Float) map.get("pitch"));
-		location.setYaw((Float) map.get("yaw"));
+		worldName = (String)map.get("world");
+		x = (Double)map.get("x");
+		y = (Double)map.get("y");
+		z = (Double)map.get("z");
+		pitch = map.containsKey("pitch") ? ((Double) map.get("pitch")).floatValue() : 0.0f;
+		yaw = map.containsKey("yaw") ? ((Double) map.get("yaw")).floatValue() : 0.0f;
+		savePitchAndYaw = map.containsKey("pitch");
 	}
-	
+
+	public String getWorldName() {
+		return worldName;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public double getZ() {
+		return z;
+	}
+
+	public float getPitch() {
+		return pitch;
+	}
+
+	public float getYaw() {
+		return yaw;
+	}
+
+	public boolean isSavePitchAndYaw() {
+		return savePitchAndYaw;
+	}
+
+	public void setSavePitchAndYaw(boolean savePitchAndYaw) {
+		this.savePitchAndYaw = savePitchAndYaw;
+	}
+
 	public Location getLocation() {
-		return location;
+		World w = Bukkit.getWorld(worldName);
+		if (w == null) {
+			throw new IllegalStateException("World not loaded");
+		}
+
+		Location loc = new Location(w, x, y, z);
+		loc.setPitch(pitch);
+		loc.setYaw(yaw);
+		return loc;
 	}
 
 	public Map<String, Object> serialize() {
 		Map<String,Object> map = new HashMap<String, Object>();
-		map.put("world", location.getWorld().getName());
-		map.put("x", location.getX());
-		map.put("y", location.getY());
-		map.put("z", location.getZ());
-		map.put("pitch", location.getPitch());
-		map.put("yaw", location.getYaw());
+		map.put("world", worldName);
+		map.put("x", x);
+		map.put("y", y);
+		map.put("z", z);
+		if (savePitchAndYaw) {
+			map.put("pitch", pitch);
+			map.put("yaw", yaw);
+		}
 		return map;
 	}
 
