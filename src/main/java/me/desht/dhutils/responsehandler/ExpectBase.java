3c443859ea1668204a883f95890e65bf5907f51c
Des Herriott <des.herriott@gmail.com>
2012-06-25 12:53:11 +0100

    The ResponseHandler API has been redone (API breakage) - Uses name Strings rather than Player objects - Add handleResponse() and cancelResponse() to ExpectBase - ExpectBase now tracks player name and response handler object - No more need to cast the return of getAction()

--- src/main/java/me/desht/dhutils/responsehandler/ExpectBase.java
+++ src/main/java/me/desht/dhutils/responsehandler/ExpectBase.java
@@ -1,9 +1,34 @@
 package me.desht.dhutils.responsehandler;
 
-import org.bukkit.entity.Player;
-
 public abstract class ExpectBase {
 
-	public abstract void doResponse(Player p);
+	private ResponseHandler resp;
+	private String playerName;
+	
+	public abstract void doResponse(String playerName);
+
+	public ResponseHandler getResp() {
+		return resp;
+	}
+
+	public void setResp(ResponseHandler resp) {
+		this.resp = resp;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public void handleAction() {
+		resp.handleAction(playerName, getClass());
+	}
+	
+	public void cancelAction() {
+		resp.cancelAction(playerName, getClass());
+	}
 
 }
