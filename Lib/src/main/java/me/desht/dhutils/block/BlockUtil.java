18c46e14c8302d9b75ed283ac5d4c4353f6aae27
Des Herriott <des.herriott@gmail.com>
2013-07-18 08:20:04 +0100

    Finish implementation of getNearestFace()

--- Lib/src/main/java/me/desht/dhutils/block/BlockUtil.java
+++ Lib/src/main/java/me/desht/dhutils/block/BlockUtil.java
@@ -10,12 +10,26 @@ public class BlockUtil {
 		BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST, BlockFace.DOWN, BlockFace.UP
 	};
 
+	/**
+	 * Get the nearest face on the given block relative to the given location.
+	 *
+	 * @param block
+	 * @param loc
+	 * @return
+	 */
 	public static BlockFace getNearestFace(Block block, Location loc) {
-		Vector v = loc.toVector().subtract(block.getLocation().toVector());
+		Location bLoc = block.getLocation().add(0.5, 0.5, 0.5);
+		Vector v = loc.toVector().subtract(bLoc.toVector());
+		float min = Float.MAX_VALUE;
+		BlockFace wantedFace = null;
 		for (BlockFace face : allFaces) {
 			Vector v1 = new Vector(face.getModX(), face.getModY(), face.getModZ());
-			System.out.println("angle " + face + " = " + v1.angle(v));
+			float angle = v.angle(v1);
+			if (angle < min) {
+				min = angle;
+				wantedFace = face;
+			}
 		}
-		return null;
+		return wantedFace;
 	}
 }
