54d431723ba877f7d2be77ddb6c49ad8ebc55dfd
Ryan Michela <deltahat@gmail.com>
2014-05-01 22:50:59 -0700

    Improving block edit speed

---
+++ Lib/src/main/java/me/desht/dhutils/ReflectionUtil.java
@@ -0,0 +1,101 @@
+package me.desht.dhutils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class ReflectionUtil {
+    public static void setProtectedValue(Object o, String field, Object newValue) {
+        setProtectedValue(o.getClass(), o, field, newValue);
+    }
+
+    public static void setProtectedValue(Class c, String field, Object newValue) {
+        setProtectedValue(c, null, field, newValue);
+    }
+
+    public static void setProtectedValue(Class c, Object o, String field, Object newValue) {
+        try {
+
+            Field f = c.getDeclaredField(field);
+
+            f.setAccessible(true);
+
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(f, f.getModifiers() & ~Modifier.FINAL);
+
+            f.set(o, newValue);
+        } catch (NoSuchFieldException ex) {
+            System.out.println("*** " + c.getName() + ":" + ex);
+        } catch (IllegalAccessException ex) {
+            System.out.println("*** " + c.getName() + ":" + ex);
+        }
+    }
+
+    public static <T> T getProtectedValue(Object obj, String fieldName) {
+        try {
+            Class c = obj.getClass();
+            while(c != Object.class) {
+                Field[] fields = c.getDeclaredFields();
+                for(Field f : fields) {
+                    if (f.getName() == fieldName) {
+                        f.setAccessible(true);
+                        return (T) f.get(obj);
+                    }
+                }
+                c = c.getSuperclass();
+            }
+            System.out.println("*** " + obj.getClass().getName() + ":No such field");
+            return null;
+        } catch (Exception ex) {
+            System.out.println("*** " + obj.getClass().getName() + ":" + ex);
+            return null;
+        }
+    }
+
+    public static <T> T getProtectedValue(Class c, String field) {
+        try {
+            Field f = c.getDeclaredField(field);
+            f.setAccessible(true);
+            return (T) f.get(c);
+        } catch (Exception ex) {
+            System.out.println("*** " + c.getName() + ":" + ex);
+            return null;
+        }
+    }
+
+    public static Object invokeProtectedMethod(Class c, String method, Object... args) {
+        return invokeProtectedMethod(c, null, method, args);
+    }
+
+    public static Object invokeProtectedMethod(Object o, String method, Object... args) {
+        return invokeProtectedMethod(o.getClass(), o, method, args);
+    }
+
+    public static Object invokeProtectedMethod(Class c, Object o, String method, Object... args) {
+        try {
+            Class[] pTypes = new Class[args.length];
+            for(int i = 0; i < args.length; i++) {
+                if (args[i] instanceof Integer) {
+                    pTypes[i] = int.class;
+                } else {
+                    pTypes[i] = args[i].getClass();
+                }
+            }
+
+            Method m = c.getDeclaredMethod(method, pTypes);
+            m.setAccessible(true);
+            return m.invoke(o, args);
+        }
+        catch (InvocationTargetException ex) {
+            System.out.println("*** " + c.getName() + "." + method + "(): " + ex.getTargetException());
+            return null;
+        }
+        catch (Exception ex) {
+            System.out.println("*** " + c.getName() + "." + method + "(): " + ex);
+            return null;
+        }
+    }
+}
+
--- v1_7_R1/src/main/java/me/desht/dhutils/nms/v1_7_R1/NMSHandler.java
+++ v1_7_R1/src/main/java/me/desht/dhutils/nms/v1_7_R1/NMSHandler.java
@@ -1,16 +1,12 @@
 package me.desht.dhutils.nms.v1_7_R1;
 
-import net.minecraft.server.v1_7_R1.Block;
-import net.minecraft.server.v1_7_R1.Chunk;
-import net.minecraft.server.v1_7_R1.ChunkCoordIntPair;
-import net.minecraft.server.v1_7_R1.EnumSkyBlock;
-
+import me.desht.dhutils.nms.api.NMSAbstraction;
+import net.minecraft.server.v1_7_R1.*;
 import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_7_R1.CraftChunk;
 import org.bukkit.craftbukkit.v1_7_R1.CraftWorld;
 import org.bukkit.craftbukkit.v1_7_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
-
-import me.desht.dhutils.nms.api.NMSAbstraction;
 import org.bukkit.util.Vector;
 
 public class NMSHandler implements NMSAbstraction {
@@ -19,8 +15,93 @@ public class NMSHandler implements NMSAbstraction {
 	public boolean setBlockFast(World world, int x, int y, int z, int blockId, byte data) {
 		net.minecraft.server.v1_7_R1.World w = ((CraftWorld) world).getHandle();
 		Chunk chunk = w.getChunkAt(x >> 4, z >> 4);
-		return chunk.a(x & 0x0f, y, z & 0x0f, Block.e(blockId), data);
+		return a(chunk, x & 0x0f, y, z & 0x0f, Block.e(blockId), data);
 	}
+
+    private boolean a(Chunk that, int i, int j, int k, Block block, int l) {
+        Block block1 = that.getType(i, j, k);
+        int k1 = that.getData(i, j, k);
+
+        if (block1 == block && k1 == l) {
+            return false;
+        } else {
+            ChunkSection chunksection = ((ChunkSection[])ReflectionUtil.getProtectedValue(that, "sections"))[j >> 4];
+
+            if (chunksection == null) {
+                if (block == Blocks.AIR) {
+                    return false;
+                }
+
+                chunksection = ((ChunkSection[])ReflectionUtil.getProtectedValue(that, "sections"))[j >> 4] = new ChunkSection(j >> 4 << 4, !that.world.worldProvider.g);
+            }
+
+            int l1 = that.locX * 16 + i;
+            int i2 = that.locZ * 16 + k;
+
+            if (!that.world.isStatic) {
+                block1.f(that.world, l1, j, i2, k1);
+            }
+
+            // CraftBukkit start - Delay removing containers until after they're cleaned up
+            if (!(block1 instanceof IContainer)) {
+                chunksection.setTypeId(i, j & 15, k, block);
+            }
+            // CraftBukkit end
+
+            if (!that.world.isStatic) {
+                block1.remove(that.world, l1, j, i2, block1, k1);
+            } else if (block1 instanceof IContainer && block1 != block) {
+                that.world.p(l1, j, i2);
+            }
+
+            // CraftBukkit start - Remove containers now after cleanup
+            if (block1 instanceof IContainer) {
+                chunksection.setTypeId(i, j & 15, k, block);
+            }
+            // CraftBukkit end
+
+            if (chunksection.getTypeId(i, j & 15, k) != block) {
+                return false;
+            } else {
+                chunksection.setData(i, j & 15, k, l);
+
+                TileEntity tileentity;
+
+                if (block1 instanceof IContainer) {
+                    tileentity = that.e(i, j, k);
+                    if (tileentity != null) {
+                        tileentity.u();
+                    }
+                }
+
+                // CraftBukkit - Don't place while processing the BlockPlaceEvent, unless it's a BlockContainer
+                if (!that.world.isStatic && (!that.world.callingPlaceEvent || (block instanceof BlockContainer))) {
+                    block.onPlace(that.world, l1, j, i2);
+                }
+
+                if (block instanceof IContainer) {
+                    // CraftBukkit start - Don't create tile entity if placement failed
+                    if (that.getType(i, j, k) != block) {
+                        return false;
+                    }
+                    // CraftBukkit end
+
+                    tileentity = that.e(i, j, k);
+                    if (tileentity == null) {
+                        tileentity = ((IContainer) block).a(that.world, l);
+                        that.world.setTileEntity(l1, j, i2, tileentity);
+                    }
+
+                    if (tileentity != null) {
+                        tileentity.u();
+                    }
+                }
+
+                that.n = true;
+                return true;
+            }
+        }
+    }
 
 	@Override
 	public void forceBlockLightLevel(World world, int x, int y, int z, int level) {
@@ -46,6 +127,35 @@ public class NMSHandler implements NMSAbstraction {
 
 	@Override
 	public void recalculateBlockLighting(World world, int x, int y, int z) {
+        Chunk chunk = ((CraftChunk)world.getChunkAt(x >> 4, z >> 4)).getHandle();
+        int i = x & 0x0f;
+        int j = y;
+        int k = z & 0x0f;
+
+        int i1 = k << 4 | i;
+        if (j >= chunk.b[i1] - 1) {
+            chunk.b[i1] = -999;
+        }
+
+        int maxY = chunk.heightMap[i1];
+
+        chunk.initLighting();
+
+        Block block = chunk.getType(i, j, k);
+        int j2 = block.k();
+
+        if (j2 > 0) {
+            if (j >= maxY) {
+                ReflectionUtil.invokeProtectedMethod(chunk, "h", i, j + 1, k);
+            }
+        } else if (j == maxY - 1) {
+            ReflectionUtil.invokeProtectedMethod(chunk, "h",i, j, k);
+        }
+
+        if (chunk.getBrightness(EnumSkyBlock.SKY, i, j, k) > 0 || chunk.getBrightness(EnumSkyBlock.BLOCK, i, j, k) > 0) {
+            ReflectionUtil.invokeProtectedMethod(chunk, "e", i, k);
+        }
+
 		net.minecraft.server.v1_7_R1.World w = ((CraftWorld) world).getHandle();
 		w.t(x, y, z);
 	}
