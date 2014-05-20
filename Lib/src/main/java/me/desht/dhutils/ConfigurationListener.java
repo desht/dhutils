797fe7e5bcbafc2f4caa9db72961a5905fba97a3
Des Herriott <des.herriott@gmail.com>
2014-05-20 17:12:35 +0100

    Update ConfigurationManager - API breakage onConfigurationValidate now returns Object  - can now modify the new value Version => 2.15.0-SNAPSHOT

--- Lib/src/main/java/me/desht/dhutils/ConfigurationListener.java
+++ Lib/src/main/java/me/desht/dhutils/ConfigurationListener.java
@@ -1,6 +1,27 @@
 package me.desht.dhutils;
 
 public interface ConfigurationListener {
-	public void onConfigurationValidate(ConfigurationManager configurationManager, String key, Object oldVal, Object newVal);
+    /**
+     * Validate and possibly modify or veto a potential change to this configuration.
+     * <p>
+     * Simply return newVal to proceed with the proposed configuration change.
+     *
+     * @param configurationManager the ConfigurationManager object
+     * @param key the configuration key
+     * @param oldVal the previous configuration value
+     * @param newVal the proposed new configuration value
+     * @return the new configuration value, possibly different from {@code newVal}
+     * @throws DHUtilsException to prevent the change from occurring
+     */
+	public Object onConfigurationValidate(ConfigurationManager configurationManager, String key, Object oldVal, Object newVal);
+
+    /**
+     * Called when a change has been made to this configuration.
+     *
+     * @param configurationManager the ConfigurationManager object
+     * @param key the configuration key
+     * @param oldVal the previous configuration value
+     * @param newVal the new configuration value
+     */
 	public void onConfigurationChanged(ConfigurationManager configurationManager, String key, Object oldVal, Object newVal);
 }
