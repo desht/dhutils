c8846942cee302cda5607a3ad61417a3ee98edbf
Des Herriott <des.herriott@gmail.com>
2014-06-27 08:32:17 +0100

    Javadocs for ItemNames

--- Lib/src/main/java/me/desht/dhutils/ItemNames.java
+++ Lib/src/main/java/me/desht/dhutils/ItemNames.java
@@ -505,6 +505,13 @@ public class ItemNames {
             .put("2267", "Music Disk (wait)")
             .build();
 
+    /**
+     * Given an item stack, return a friendly printable name for the item, as
+     * the (English-language) vanilla Minecraft client would display it.
+     *
+     * @param stack the item stack
+     * @return a friendly printable name for the item
+     */
     public static String lookup(ItemStack stack) {
         if (stack.hasItemMeta()) {
             ItemMeta meta = stack.getItemMeta();
@@ -536,6 +543,15 @@ public class ItemNames {
         return result;
     }
 
+    /**
+     * Given an item stack return a friendly name for the item, in the form
+     * "{amount} x {item-name}" where {amount} is the number of items in the
+     * stack and {item-name} is the return value of
+     * {@link #lookup(org.bukkit.inventory.ItemStack)}}.
+     *
+     * @param stack the item stack
+     * @return a friendly printable name for the item, with amount information
+     */
     public static String lookupWithAmount(ItemStack stack) {
         String s = lookup(stack);
         return stack.getAmount() + " x " + s;
